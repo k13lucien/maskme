c1ccfadafc684e4cf24733fab71840cb6f727cb2
Lucien Kiemde <lucienkiemde4@gmail.com>
2026-04-28 00:02:45 +0000

    feat(strategies): add redaction strategy with partial/total masking support

--- src/maskme/strategies/__init__.py
+++ src/maskme/strategies/__init__.py
@@ -1,6 +1,8 @@
 from . import hashing
+from . import redaction
 
 # Registry of available strategies
 STRATEGIES = {
-    "hash": hashing.apply
+    "hash": hashing.apply,
+    "redact": redaction.apply,
 }
